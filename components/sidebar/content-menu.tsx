09e497c46a138688f7084b985f58cafda00e7316
dominicpam89 <dominicpam89@gmail.com>
2024-12-26 19:07:35 +0700

    UI: - update content-menu.tsx to use new child components - create generic child / list item to be able to be extended

--- components/sidebar/content-menu.tsx
+++ components/sidebar/content-menu.tsx
@@ -1,12 +1,22 @@
+"use client"
+import { useStoreMock } from "@/lib/store/index.mock"
+import ContentMenuChild from "./content-menu/child"
+
 export default function SidebarContentMenu() {
+   const { menus } = useStoreMock()
+   const displayedMenus = menus[0].children[0]
    return (
       <ul
          aria-label="content-menu"
          className="w-full flex flex-col gap-2 text-blue-gray-white"
       >
-         <li>Systems</li>
-         <li>Users Group</li>
-         <li>Competition</li>
+         {displayedMenus.children.map((item) => {
+            return (
+               <ContentMenuChild key={item.menuId} item={item}>
+                  <p>Some children if needed</p>
+               </ContentMenuChild>
+            )
+         })}
       </ul>
    )
 }
